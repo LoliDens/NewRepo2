f9b85d59acc1b10ffd675f45ea5c3f7793cc21f5
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-19 20:28:15 +0500

    Изменен вывод сообщения

--- homeWork_minPow/Program.cs
+++ homeWork_minPow/Program.cs
@@ -16,18 +16,17 @@ namespace homeWork_minPow
             int lowerLimitRandom = 0;
             int upperLimitRandom = 101;
             int number = random.Next(lowerLimitRandom, upperLimitRandom);
-            int twoOnThePower = 1;
+            int numberOnThePower = 1;
             int numberToExponentiate = 2;
 
-            while (twoOnThePower < number) 
+            while (numberOnThePower < number) 
             {
-                twoOnThePower *= numberToExponentiate;
+                numberOnThePower *= numberToExponentiate;
                 numberDigree++;
             }
 
-            Console.WriteLine($"Для числа {number} минимальная степень {numberToExponentiate}, это {numberToExponentiate} в {numberDigree} степени");
+            Console.WriteLine($"Для числа {number} < {numberOnThePower}, где {numberOnThePower}, это {numberToExponentiate} в {numberDigree} степени");
             Console.ReadKey();
-
         }   
     }
 }
